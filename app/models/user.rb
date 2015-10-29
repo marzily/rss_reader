5cee36f652bd3cf5199b5589cf1c103228346ba8
margie <ly.margarett@gmail.com>
2015-10-28 23:20:33 -0700

    modify line

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
                     uniqueness: true,
                     format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\z/,
                               message: "Must be a valid email address." }
-                              
+
   validates :password, :password_confirmation, presence: true,
                                                length: { minimum: 8 }
 end
