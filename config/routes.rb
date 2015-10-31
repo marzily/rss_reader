c05959a41c0b718a238b4d3d899606f6107186a0
margie <ly.margarett@gmail.com>
2015-10-31 10:58:18 -0700

    feed properly saves and deletes from db upon favoriting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'sessions#new'
 
-  post    '/login', to: 'sessions#create'
+  post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
   resources :users, only: [:new, :create]
